f5e5844e8e61747ba549b16fe26e1eb793d697ff
Marcus Fihlon <marcus@fihlon.ch>
2016-11-14 17:02:35 +0100

    #5 Extend test coverage of the ping service

--- src/test/java/ch/sportchef/ping/controller/PongServiceTest.java
+++ src/test/java/ch/sportchef/ping/controller/PongServiceTest.java
@@ -18,7 +18,12 @@
 package ch.sportchef.ping.controller;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 
@@ -26,7 +31,11 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.not;
 import static org.hamcrest.Matchers.startsWith;
+import static org.powermock.api.mockito.PowerMockito.mockStatic;
+import static org.powermock.api.mockito.PowerMockito.when;
 
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(PongService.class)
 public class PongServiceTest {
 
     @Test
@@ -42,6 +51,41 @@ public class PongServiceTest {
         assertThat(pong, containsString(" at " + timeStr));
         assertThat(pong, not(startsWith("Pong from null")));
         assertThat(pong, startsWith("Pong from "));
+    }
+
+    @Test
+    public void getPongWithoutEnvironmentVariable() {
+        // arrange
+        mockStatic(System.class);
+        when(System.getenv("HOSTNAME")).thenReturn(null);
+        final PongService pongService = new PongService();
+
+        // act
+        final String pong = pongService.getPong();
+
+        // assert
+        final String timeStr = ZonedDateTime.now().format(DateTimeFormatter.RFC_1123_DATE_TIME);
+        assertThat(pong, containsString(" at " + timeStr));
+        assertThat(pong, not(startsWith("Pong from null")));
+        assertThat(pong, startsWith("Pong from "));
+    }
+
+    @Test
+    public void getPongWithUnknownHostException() throws UnknownHostException {
+        // arrange
+        mockStatic(System.class);
+        when(System.getenv("HOSTNAME")).thenReturn(null);
+        mockStatic(InetAddress.class);
+        when(InetAddress.getLocalHost()).thenThrow(new UnknownHostException());
+        final PongService pongService = new PongService();
+
+        // act
+        final String pong = pongService.getPong();
+
+        // assert
+        final String timeStr = ZonedDateTime.now().format(DateTimeFormatter.RFC_1123_DATE_TIME);
+        assertThat(pong, containsString(" at " + timeStr));
+        assertThat(pong, startsWith("Pong from unknown "));
     }
 
 }
